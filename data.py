202d58e19c0021ef3047ce761b1c59b65a4c24ca
Atharva Kokate <61040145+atharvakokatee@users.noreply.github.com>
2020-02-21 12:10:24 +0530

    Add routes and home page

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article One',
+            'body':'Lorem ipsum djfnsdjfn sjdnsjdns sjdnsjns jdnsjdns jsndjsdns jsndsjdnsj sjdnsjdnsj sndsj',
+            'author':'Atharva Kokate',
+            'create_date':'20-02-2020'
+        },
+        {
+            'id':1,
+            'title':'Article Two',
+            'body':'Lorem ipsum djfnsdjfn sjdnsjdns sjdnsjns jdnsjdns jsndjsdns jsndsjdnsj sjdnsjdnsj sndsj',
+            'author':'Atharva Kokate',
+            'create_date':'20-02-2020'
+        },
+        {
+            'id':1,
+            'title':'Article Three',
+            'body':'Lorem ipsum djfnsdjfn sjdnsjdns sjdnsjns jdnsjdns jsndjsdns jsndsjdnsj sjdnsjdnsj sndsj',
+            'author':'Atharva Kokate',
+            'create_date':'20-02-2020'
+        }
+    ]
+    return articles
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Python Flask
+
+* Flask is a micro framework which means that it has very little dependencied to external libraries and is very light
+
+pip install flask-mysqldb //module to communicate with mysql
+pip install flask-wtf
+//module to help with form validation, diff types of field
+pip install passlib
+//help with hashing our passwords
+
+### Registration
